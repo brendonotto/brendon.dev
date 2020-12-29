66462cd5aeed59035ba8e9dfff304e44d54f2059
Brendon Otto <brendon@brendonotto.com>
2020-12-29 08:18:55 -0600

    Added remark to render markdown, date component and formatting

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,19 @@
 import Layout from '../../components/layout'
+import Date from '../../components/date'
+import Head from 'next/head'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({postData}) {
     return (
         <Layout>
-            {postData.title}
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <h1>{postData.title}</h1>
             <br />
-            {postData.id}
+            <Date dateString={postData.date} />
             <br />
-            {postData.date}
+            <div dangerouslySetInnerHTML={{__html: postData.contentHtml }} />
         </Layout>
     )
 }
@@ -22,7 +27,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({params}) {
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
     return {
         props: {
             postData
