67cedf45d5a4584fb3a011fbe65707d5000a5d24
Brendon Otto <brendon@brendonotto.com>
2021-01-10 08:19:18 -0600

    4; Added tailwindcss, migrated layout module to tailwind

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 
 const name = "Brendon Otto"
@@ -9,7 +8,7 @@ export const siteTitle = "Brendon's Blog"
 
 export default function Layout({ children, home }) {
     return (
-        <div className={styles.container}>
+        <div className="max-w-xl py-0 px-4 my-12 mx-auto mb-24">
             <Head>
                 <link rel="icon" href="/favicon.ico" />
                 <meta 
@@ -23,14 +22,14 @@ export default function Layout({ children, home }) {
                 <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
             </Head>
-            <header className={styles.header}>
+            <header className="flex flex-col items-center">
                 {home ? (
                     <>
                         <Image
                             src="/images/profile.jpg"
                             height={157}
                             width={96}
-                            className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+                            className={`w-32 h-32 ${utilStyles.borderCircle}`}
                             alt={name}
                         />
                         <h1 className={utilStyles.heading2X1}>{name}</h1>
@@ -41,7 +40,7 @@ export default function Layout({ children, home }) {
                                 <a>
                                     <img
                                         src="/images/profile.jpg"
-                                        className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                                        className={`w-24 h-24 ${utilStyles.borderCircle}`}
                                         alt={name}
                                     />
                                 </a>
@@ -57,7 +56,7 @@ export default function Layout({ children, home }) {
             </header>
             <main>{children}</main>
             {!home && (
-                <div className={styles.backToHome}>
+                <div className="mt-12 mx-0 mb-0">
                     <Link href="/">
                         <a>← Back to home</a>
                     </Link>
